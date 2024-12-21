7ab407a042ed960c8ca0d1bd3fd3f597542bee70
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2024-12-21 16:38:29 +0530

    Open-Closed Principle

--- OopAndDesignPatterns/Program.cs
+++ OopAndDesignPatterns/Program.cs
@@ -1,5 +1,6 @@
 ﻿//using OopAndDesignPatterns.OOPPrinciples.Inheritance;
-using OopAndDesignPatterns.OOPPrinciples.Polymorphism;
+//using OopAndDesignPatterns.OOPPrinciples.Polymorphism;
+using OopAndDesignPatterns.SOLIDPrinciples.OpenClosed;
 
 ////Inheritance
 //Console.WriteLine("Inheritance");
@@ -14,7 +15,7 @@ using OopAndDesignPatterns.OOPPrinciples.Polymorphism;
 //Bike bike = new();
 //bike.Brand = "Haya";
 
-Console.WriteLine("Polymorphism");
+//Console.WriteLine("Polymorphism");
 /*
 List<object> vehicles = new();
 vehicles.Add(new OopAndDesignPatterns.OOPPrinciples.Polymorphism.Car { Brand = "Toyota", Model = "Sample", NumberOfDoors = 6, Year = 1992, });
@@ -38,13 +39,32 @@ foreach (var vehicle in vehicles)
 */
 //Using above approach, we will have to modify code for everytime we add a new Vehicle type.
 //So, if we want to avoid this. Instead of giving a generic object. We can define it with Parent class type. Demonstrated below...
-List<Vehicle> vehicles =
-[
-    new Car { Brand = "Toyota", Model = "Sample", NumberOfDoors = 6, Year = 1992, },
-    new Motorcycle { Brand = "Harley", Model = "Test", Year = 2000 },
-    new Vehicle{Brand = "Vehicle", Model = "Test3", Year = 2020},
-];
-foreach (var vehicle in vehicles)
-{
-    vehicle.Start(); // Runtime polymorphism //Dynamically checks the type of object and invokes method calling. This is where virtual and override comes into action. 
-}
+//List<Vehicle> vehicles =
+//[
+//    new Car { Brand = "Toyota", Model = "Sample", NumberOfDoors = 6, Year = 1992, },
+//    new Motorcycle { Brand = "Harley", Model = "Test", Year = 2000 },
+//    new Vehicle{Brand = "Vehicle", Model = "Test3", Year = 2020},
+//];
+//foreach (var vehicle in vehicles)
+//{
+//    vehicle.Start(); // Runtime polymorphism //Dynamically checks the type of object and invokes method calling. This is where virtual and override comes into action. 
+//}
+
+
+//SOLID
+//Single Responsibilty Principle
+
+//Open Closed Principle
+//Software entities - Functions/Modules/classes should be open for extension but closed for modification
+Shape shape = new();
+shape.Length = 10;
+shape.Width = 20;
+shape.Radius = 5;
+shape.ShapeType = ShapeType.Circle;
+shape.ShapeType = ShapeType.Rectangle;
+
+var res = shape.CalculateArea();
+Console.WriteLine(res);
+
+//Liskov Substitution Principle
+//Objects of sub class should be able to substitute objects of superclass without affecting the correctness of the program
---
+++ OopAndDesignPatterns/SOLIDPrinciples/OpenClosed/Shape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopAndDesignPatterns.SOLIDPrinciples.OpenClosed
+{
+    public class Shape
+    {
+        public ShapeType ShapeType { get; set; }
+        public double Radius { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double CalculateArea()
+        {
+            switch(ShapeType)
+            {
+                case ShapeType.Circle: 
+                    return Math.PI * Math.Pow(Radius, 2);
+                case ShapeType.Rectangle:
+                    return Length * Width;
+                default:
+                    throw new NotSupportedException("Unsupported shape");
+            }
+        }
+    }
+}
