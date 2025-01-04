cb52757703da94ee54bec552949782bbb31b9dcd
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-01-05 00:11:27 +0530

    Abstract Class

---
+++ OOPConcepts/Program.cs
@@ -0,0 +1,17 @@
+﻿abstract class Customer //Can only be used as base class. Cant be instantiated.
+{
+    public abstract void Print();
+}
+class Customer1 : Customer
+{
+    public override void Print()
+    {
+        Console.WriteLine("Print method");
+    }
+    static void Main()
+    {
+        //Customer1 c = new();
+        Customer c = new Customer1(); //Works same as above line
+        c.Print();
+    }
+}
