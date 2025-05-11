92d6231220324b4f656817189d1d033134c8d857
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-05-11 18:51:59 +0530

    Swap 2 numbers without 3rd variable

--- OopAndDesignPatterns/DSA/Palindrome/Palindrome/Program.cs
+++ OopAndDesignPatterns/DSA/Palindrome/Palindrome/Program.cs
@@ -1,6 +1,18 @@
-﻿bool IsPalindrome(string input)
+﻿//bool IsPalindrome(string input)
+//{
+//    string reversed =  new(input.Reverse().ToArray());
+//    return input.Equals(reversed, StringComparison.OrdinalIgnoreCase);
+//}
+//Console.WriteLine(IsPalindrome("madam"));
+
+
+void SwapNumbers(ref int a , ref int b) //Swapping without a third variable
 {
-    string reversed =  new(input.Reverse().ToArray());
-    return input.Equals(reversed, StringComparison.OrdinalIgnoreCase);
+    a = a + b;
+    a = a + b;
+    b = a - b;
+    a = a - b;
 }
-Console.WriteLine(IsPalindrome("madam"));
+int x = 5; int y = 10;
+SwapNumbers(ref x, ref y);
+Console.WriteLine($"{x}, {y}");
