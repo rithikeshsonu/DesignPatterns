d1a288a18ffcc10e3999b052fb7c91a401f96471
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-05-11 18:53:32 +0530

    Update Program.cs

--- OopAndDesignPatterns/DSA/Palindrome/Palindrome/Program.cs
+++ OopAndDesignPatterns/DSA/Palindrome/Palindrome/Program.cs
@@ -1,14 +1,13 @@
-﻿//bool IsPalindrome(string input)
-//{
-//    string reversed =  new(input.Reverse().ToArray());
-//    return input.Equals(reversed, StringComparison.OrdinalIgnoreCase);
-//}
-//Console.WriteLine(IsPalindrome("madam"));
-
-
-void SwapNumbers(ref int a , ref int b) //Swapping without a third variable
+﻿bool IsPalindrome(string input)
 {
-    a = a + b;
+    string reversed = new(input.Reverse().ToArray());
+    return input.Equals(reversed, StringComparison.OrdinalIgnoreCase);
+}
+Console.WriteLine(IsPalindrome("madam"));
+
+
+void SwapNumbers(ref int a, ref int b) //Swapping without a third variable
+{
     a = a + b;
     b = a - b;
     a = a - b;
