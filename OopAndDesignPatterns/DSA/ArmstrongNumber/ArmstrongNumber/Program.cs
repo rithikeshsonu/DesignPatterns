f5520bf6fd927f89d9231e3a4cbfe8c127a7536f
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-06-30 23:34:43 +0530

    Check if Armstrong

---
+++ OopAndDesignPatterns/DSA/ArmstrongNumber/ArmstrongNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿void IsArmstrongNumber(int num)
+{
+    int temp, rem;
+    double sum = 0;
+    temp = num;
+    while (num > 0) //153
+    {
+        rem = num % 10; //3
+        sum += Math.Pow(rem, 3);
+        num /= 10;
+    }
+    if (sum == temp)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
+IsArmstrongNumber(153);
