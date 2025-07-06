e957c5379d6e34db3350130bd4dc265cecc77d99
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-07-06 20:18:14 +0530

    LINQ - Intermediate

--- OopAndDesignPatterns/OOPPrinciples/LINQ/LINQ/Program.cs
+++ OopAndDesignPatterns/OOPPrinciples/LINQ/LINQ/Program.cs
@@ -9,3 +9,21 @@ var countNumsGreaterThan5 = numbers.Count(x => x > 5);
 var ascending = numbers.Order();
 var descending = numbers.OrderByDescending(x => x);
 var unique = numbers.Distinct().ToList();
+var firstEven = numbers.First(x => x % 2 == 0);
+var isAnyNumberGreaterThan20 = numbers.Any(x => x > 1);
+var IsAllNumbersPositive = numbers.All(x => x > 0);
+string[] words = ["Apple", "Banana", "Avocado", "Mango"];
+var aWords = words.Where(x => x.StartsWith('A')).ToList();
+var grouped = words.GroupBy(x => x.Length);
+var first3 = words.Take(3);
+var skip2 = words.Skip(2);
+
+List<string> list1 = ["A", "B"];
+List<string> list2 = ["B", "C"];
+
+var intersect = list1.Intersect(list2);
+var union = list1.Union(list2);
+var except = list1.Except(list2);
+
+
+var res = aWords;
